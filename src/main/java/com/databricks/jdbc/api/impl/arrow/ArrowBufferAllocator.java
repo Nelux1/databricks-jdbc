ffd6c1cba7f9d9cba0afc20b0041112d309eba9e
tejassp-db <241722411+tejassp-db@users.noreply.github.com>
2025-12-18 09:49:13 +0530

    PECOBLR-1121 Use Arrow patch as fallback.
    Use native Arrow if available. Otherwise fallback to the patch version.

---
+++ src/main/java/com/databricks/jdbc/api/impl/arrow/ArrowBufferAllocator.java
@@ -0,0 +1,56 @@
+package com.databricks.jdbc.api.impl.arrow;
+
+import com.databricks.jdbc.log.JdbcLogger;
+import com.databricks.jdbc.log.JdbcLoggerFactory;
+import org.apache.arrow.memory.BufferAllocator;
+import org.apache.arrow.memory.DatabricksBufferAllocator;
+import org.apache.arrow.memory.RootAllocator;
+
+/**
+ * Creates {@link BufferAllocator} instances.
+ *
+ * <p>First tries to create a {@link RootAllocator} which uses off-heap memory and is faster. If
+ * that fails (usually due to JVM reflection restrictions), falls back to {@link
+ * DatabricksBufferAllocator} which uses heap memory.
+ */
+public class ArrowBufferAllocator {
+  /** Can a {@code RootAllocator} be created in this JVM instance? */
+  private static final boolean canCreateRootAllocator;
+
+  /** Logger instance. */
+  private static final JdbcLogger LOGGER = JdbcLoggerFactory.getLogger(ArrowBufferAllocator.class);
+
+  /* Check if the RootAllocator can be instantiated. */
+  static {
+    RootAllocator rootAllocator = null;
+    try {
+      rootAllocator = new RootAllocator();
+    } catch (Throwable t) {
+      LOGGER.info(
+          "Failed to create RootAllocator, will use DatabricksBufferAllocator as fallback: "
+              + t.getMessage());
+    }
+
+    canCreateRootAllocator = rootAllocator != null;
+    if (rootAllocator != null) {
+      try {
+        rootAllocator.close();
+      } catch (Throwable t) {
+        LOGGER.warn("RootAllocator could not be closed: " + t.getMessage());
+      }
+    }
+  }
+
+  /**
+   * @return an instance of the {@code BufferAllocator}.
+   */
+  public static BufferAllocator getBufferAllocator() {
+    // TODO reuse RootAllocators?
+    // TODO should this method be non-static.
+    if (canCreateRootAllocator) {
+      return new RootAllocator();
+    } else {
+      return new DatabricksBufferAllocator();
+    }
+  }
+}
